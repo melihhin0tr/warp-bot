4471c66d61499e0ae62642efd1ed968d394d41de
melihhin0tr <65651198+melihhin0tr@users.noreply.github.com>
2020-12-09 16:24:57 +0300

    Update warpy.php

--- warpy.php
+++ warpy.php
@@ -1,47 +1,25 @@
 <?php
-
 system("clear");
-
 $id = "4023d3d3-8238-4891-94df-483feb76a996";
-
 echo "\n";
-
 for ($i=1; $i <= 300; $i++) {
-
- sleep(18); // Recommend 18 second
-
- $api = curl($id);
-
- if ($api == "success") {
-
-  echo "$i.Success Send 1GB\n";
-
- } else {
-
-  echo "$i.Failed Send 1GB\n";
-
- }
-
+	sleep(18); // Recommend 18 second
+	$api = curl($id);
+	if ($api == "success") {
+		echo "$i.Success Send 1GB\n";
+	} else {
+		echo "$i.Failed Send 1GB\n";
+	}
 }
 
 function curl($id) {
-
- $ch = curl_init();
-
- curl_setopt($ch, CURLOPT_URL, "https://api.nabil.my.id/warp");
-
- curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
- curl_setopt($ch, CURLOPT_POST, 1);
-
- curl_setopt($ch, CURLOPT_POSTFIELDS, 'id='.$id.'');
-
- $output = curl_exec($ch);
-
- curl_close($ch);
-
- return $output;
-
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, "https://api.nabil.my.id/warp");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, 'id='.$id.'');
+	$output = curl_exec($ch);
+	curl_close($ch);
+	return $output;
 }
-
 ?>
